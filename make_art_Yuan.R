829f143592c2fd74e68b1f1229549dccd999d063
Luiza12575275 <123747281+Luiza12575275@users.noreply.github.com>
2023-01-27 16:58:12 +0100

    individual R files for each function

---
+++ make_art_Yuan.R
@@ -0,0 +1,15 @@
+## make_art() function
+
+create_random_abstract_art <- function(seed) {
+  set.seed(seed)
+  data <- data.frame(x = runif(1000, min = -100, max = 100) + runif(5, min = -100, max = 100),
+                     y = runif(1000, min = -100, max = 100) + runif(5, min = -100, max = 100),
+                     color = sample(brewer.pal(n = 8, name = "Dark2"), 1000, replace = TRUE))
+  ggplot(data, aes(x, y, color = color)) +
+    geom_line(aes(group = cut_number(..x.., 5), fill = color),alpha = 0.8) +
+    geom_path(aes(group = cut_number(..x.., 5)), size = 0.5) +
+    theme_void() +
+    theme(legend.position = "none")
+}
+
+#the function takes a random seed as argument and produces a piece of random art
